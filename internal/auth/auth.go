2b3bb4f9bcbf98d8050ef35085167d30b1223e84
Tristan Davis <tridav95@gmail.com>
2025-06-14 17:47:10 -0500

    ch6

--- internal/auth/auth.go
+++ internal/auth/auth.go
@@ -1,7 +1,39 @@
 package auth
 import (
 	"golang.org/x/crypto/bcrypt"
+	"github.com/golang-jwt/jwt/v5"
+	"github.com/google/uuid"
+	"time"
 )
+
+func MakeJWT(userID uuid.UUID, tokenSecret string, expiresIn time.Duration) (string, error) {
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.RegisteredClaims{
+		Issuer: "chirpy",
+		IssuedAt: jwt.NewNumericDate(time.Now().UTC()),
+		ExpiresAt: jwt.NewNumericDate(time.Now().Add(expiresIn).UTC()),
+		Subject: userID.String(),
+	})
+
+	tokenString, err := token.SignedString(tokenSecret)
+	return tokenString, err
+}
+
+func ValidateJWT(tokenString, tokenSecret string) (uuid.UUID, error) {
+	token, err := jwt.ParseWithClaims(tokenString, &jwt.RegisteredClaims{}, func(token *jwt.Token) (interface{}, error) {
+		return []byte("AllYourBase"), nil
+	})
+	var id uuid.UUID
+	if err != nil {
+		return id, err
+	} else if claims, ok := token.Claims.(*jwt.RegisteredClaims); ok {
+
+		id,_ = uuid.Parse(claims.Subject)
+	} else {
+		return id,err //log.Fatal("unknown claims type, cannot proceed")
+	}
+
+	return id,nil
+}
 
 func HashPassword(password string) (string, error) {
 	bP,err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost);
